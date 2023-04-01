d7e972e180ef8d27570df155abb1b21ac41d52f9
lordchuffnel <fs-j@live.com>
2023-03-31 19:59:35 -0700

    cleaning up app file

---
+++ src/data/defaultEdgeOptions.ts
@@ -0,0 +1,9 @@
+import { MarkerType } from "reactflow";
+export default {
+    style: { strokeWidth: 1.5, stroke: "black" },
+    type: "floating",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      color: "black"
+    }
+  };
