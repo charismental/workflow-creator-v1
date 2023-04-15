0568654a1524318c7b518901cdc840c0dd359694
YepYep <fs-j@live.com>
2023-04-15 09:53:48 -0700

    clear state modal

--- src/components/CustomControls.tsx
+++ src/components/CustomControls.tsx
@@ -1,15 +1,46 @@
 import { Controls, ControlButton } from "reactflow";
 import { FC } from "react";
-import { ClearOutlined, ReloadOutlined } from "@ant-design/icons";
+import {
+  ClearOutlined,
+  ReloadOutlined,
+  ExclamationCircleFilled,
+} from "@ant-design/icons";
 import useMainStore from "store";
+import { Modal } from "antd";
+import { shallow } from "zustand/shallow";
 
 const CustomControls: FC = () => {
   const clearStorage = useMainStore.persist.clearStorage;
   const rehydrateState = useMainStore.persist.rehydrate;
+  const [setNodes, activeProcessName] = useMainStore(
+    (state) => [state.setNodes, state.activeProcessName],
+    shallow
+  );
+
+  const deleteStateWarning = () =>
+    Modal.confirm({
+      title: "Are you sure you want to reset the state?",
+      icon: <ExclamationCircleFilled />,
+      content: (
+        <div>
+          <p>This action cannot be undone!</p>
+        </div>
+      ),
+      onCancel() {},
+      onOk() {
+        return new Promise((res, rej) => {
+          setTimeout(() => {
+            res((setNodes([], activeProcessName), clearStorage()));
+          }, 2000);
+        }).catch(() => console.log("error...."));
+      },
+      centered: true,
+      okText: "I'm Sure",
+    });
 
   return (
     <Controls>
-      <ControlButton onClick={() => clearStorage()} title="Clear State">
+      <ControlButton onClick={deleteStateWarning} title="Clear State">
         <ClearOutlined />
       </ControlButton>
       <ControlButton
--- src/hooks/useHydration.ts
+++ src/hooks/useHydration.ts
@@ -5,8 +5,6 @@ export default () => {
   const [hydrated, setHydrated] = useState(useMainStore.persist.hasHydrated);
 
   useEffect(() => {
-    // Note: This is just in case you want to take into account manual rehydration.
-    // You can remove the following line if you don't need it.
     const unsubHydrate = useMainStore.persist.onHydrate(() =>
       setHydrated(false)
     );
