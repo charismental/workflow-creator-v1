a9ccee12db0880087440a20e3dc13800e6ee8fc5
YepYep <fs-j@live.com>
2023-05-01 21:13:06 -0700

    added autocomplete box on statelist selectbox

--- src/components/StateCollapseBox.tsx
+++ src/components/StateCollapseBox.tsx
@@ -1,5 +1,5 @@
 import { PlusCircleOutlined, PlusCircleTwoTone } from "@ant-design/icons";
-import { Collapse, Divider, InputRef, Space } from "antd";
+import { Collapse, Divider, InputRef, Space, AutoComplete } from "antd";
 import React, { useRef, useState } from "react";
 import useMainStore from "store";
 import { shallow } from "zustand/shallow";
@@ -21,25 +21,31 @@ const StateCollapseBox: React.FC<StateCollapsebox> = ({
   disabled = false,
   useStyle = {},
 }) => {
-  const [name, setName] = useState("");
+  const [newStateName, setNewStateName] = useState("");
+  const [searchStateName, setSearchStateName] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
   const inputRef = useRef<InputRef>(null);
-  const [activeRole, roleColors] = useMainStore(
-    (state) => [state.activeRole, {}],
-    shallow
-  );
+  const [activeRole] = useMainStore((state) => [state.activeRole], shallow);
 
   const onDragStart = (event: any, nodeType: any) => {
     event.dataTransfer.setData("application/reactflow", nodeType);
     event.dataTransfer.effectAllowed = "move";
   };
 
-  const onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
+  const onNewStateName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewStateName(event.target.value);
   };
 
+  const handleSearch = (value: string) => {
+    return items.find((el) => el.startsWith(value));
+  };
+
+  const onSearchChange = (value: string) => {
+    setSearchStateName(value);
+  };
   const itemColor = () => {
     // return roleColors[activeRole];
-    return '#ddd'
+    return "#ddd";
   };
 
   const addNewItem = (
@@ -47,49 +53,83 @@ const StateCollapseBox: React.FC<StateCollapsebox> = ({
   ) => {
     e.preventDefault();
     addNew && addNew(name);
-    setName("");
+    setNewStateName("");
     setTimeout(() => {
       inputRef.current?.focus();
     }, 0);
   };
 
   return (
-    <Collapse
-      accordion
-      style={{ width: "100%", maxHeight: "400px", overflowY: "auto", ...useStyle }}
-      size="small"
-      expandIconPosition="end"
-    >
-      <Panel header={"Add/Select State"} key="1">
-        {items.map((item) => (
-          <div
-            key={item}
-            className={styles.stateItem}
-            style={{ backgroundColor: itemColor(), ...(disabled && { pointerEvents: 'none' }) }}
-            onMouseDown={(e) => {
-              e.stopPropagation();
-            }}
-            draggable
-            onDragStart={(event) => onDragStart(event, item)}
-          >
-            {item}
-          </div>
-        ))}
-        <>
-          {items.length > 0 && <Divider style={{ margin: "8px 0" }} />}
+    <div style={{ border: "3px solid #d4d4d4", borderRadius: "10px" }}>
+      <div>
+        <Collapse
+          onChange={() => setIsOpen(!isOpen)}
+          ghost
+          accordion
+          style={{
+            width: "100%",
+            maxHeight: "400px",
+            overflowY: "auto",
+            ...useStyle,
+          }}
+          size="small"
+          expandIconPosition="end"
+        >
+          <Panel header={"Add/Select State"} key="1">
+            <AutoComplete
+              placeholder={"Search States"}
+              allowClear
+              onSearch={(val) => handleSearch(val)}
+              onChange={onSearchChange}
+              value={searchStateName}
+              open={searchStateName.length > 0}
+              style={{ width: 200 }}
+              onSelect={(value) => console.log("selected", value)}
+              dropdownMatchSelectWidth={190}
+              options={items.map((el) => ({ value: el }))}
+              filterOption={(inputValue, item) =>
+                (item &&
+                  item.value.toUpperCase().indexOf(inputValue.toUpperCase()) !==
+                    -1) ||
+                false
+              }
+            />
+            {items.map((item) => (
+              <div
+                key={item}
+                className={styles.stateItem}
+                style={{
+                  backgroundColor: itemColor(),
+                  ...(disabled && { pointerEvents: "none" }),
+                }}
+                onMouseDown={(e) => {
+                  e.stopPropagation();
+                }}
+                draggable
+                onDragStart={(event) => onDragStart(event, item)}
+              >
+                {item}
+              </div>
+            ))}
+          </Panel>
+        </Collapse>
+      </div>
+      {isOpen && (
+        <div>
+          <Divider style={{ margin: "8px 0" }} />
           <Space style={{ padding: "0 8px 4px" }}>
             <AddNewInput
               hasColorInput={false}
-              changeEvent={onNameChange}
+              changeEvent={onNewStateName}
               placeholder="Add New State"
-              inputValue={name}
+              inputValue={newStateName}
               buttonShape="circle"
               buttonType="text"
-              disabledState={!name.length}
+              disabledState={!newStateName.length}
               addNew={addNewItem}
-              pressEnterHandler={name?.length && addNewItem}
+              pressEnterHandler={newStateName?.length && addNewItem}
               icon={
-                !name.length ? (
+                !newStateName.length ? (
                   <PlusCircleOutlined style={{ fontSize: "20px" }} />
                 ) : (
                   <PlusCircleTwoTone style={{ fontSize: "20px" }} />
@@ -97,9 +137,9 @@ const StateCollapseBox: React.FC<StateCollapsebox> = ({
               }
             />
           </Space>
-        </>
-      </Panel>
-    </Collapse>
+        </div>
+      )}
+    </div>
   );
 };
 
