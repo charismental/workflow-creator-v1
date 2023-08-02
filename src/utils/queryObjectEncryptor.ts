7578e2cc218baf1924d3e4e77422eb1172e7be46
OrangeLazarus <39839237+charismental@users.noreply.github.com>
2023-08-01 20:04:46 -0700

    Trying to compress/decompress string

--- src/utils/queryObjectEncryptor.ts
+++ src/utils/queryObjectEncryptor.ts
@@ -1,22 +1,24 @@
 import CryptoJS from 'crypto-js';
+import LZString from 'lz-string';
 import { Nullable, WorkflowRole } from 'types';
 
 const secret = process.env.REACT_APP_QUERY_SECRET || 'secret';
 
 export const queryObjectEncryptor = (stateObject: any): string => {
     if (!stateObject || typeof stateObject !== 'object') return '';
-    const stringifiedState = JSON.stringify(stateObject);
+    const compressedStringifiedState = LZString.compressToEncodedURIComponent(JSON.stringify(stateObject));
+    const encrypted = CryptoJS.AES.encrypt(compressedStringifiedState, secret).toString();
 
-    return CryptoJS.AES.encrypt(stringifiedState, secret).toString();
+    return encrypted
 };
 
 export const queryObjectDecryptor = (encryptedObjectStr: string): Nullable<WorkflowRole> => {
     if (!encryptedObjectStr || typeof encryptedObjectStr !== 'string') return null;
-    const bytes = CryptoJS.AES.decrypt(encryptedObjectStr, secret);
-    const decryptedObjectStr = bytes.toString(CryptoJS.enc.Utf8);
+    const bytes = CryptoJS.AES.decrypt(decodeURIComponent(encryptedObjectStr), secret);
+    const decryptedCompressedObjectStr = bytes.toString(CryptoJS.enc.Utf8);
 
     try {
-        const parsedObject = JSON.parse(decryptedObjectStr);
+        const parsedObject = JSON.parse(LZString.decompressFromEncodedURIComponent(decryptedCompressedObjectStr));
 
         return parsedObject
     } catch (e) {
